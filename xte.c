f48e07855205feafc58c1dd4b6bda8af98ba8a29
jojodicus <jojodicus@gmail.com>
2022-11-09 09:29:07 +0100

    less chances to leak input

--- xte.c
+++ xte.c
@@ -96,17 +96,20 @@ void check_password(const char* pname, int uid) {
     printf("[%s] - password: ", pname);
     fflush(stdout);
 
-    /* read password input */
-    char input[BUFFERSIZE];
-    read_password(input);
-    putchar('\n');
-    fflush(stdout);
 
-    /* get hashed user pw and hash input */
+    /* get hashed user pw */
     struct passwd* pw = getpwuid(uid);
    	struct spwd* shadowEntry = getspnam(pw->pw_name);
+
+    /* read password input and hash it */
+    char input[BUFFERSIZE];
+    read_password(input);
     char* hashed_pw = crypt(input, shadowEntry->sp_pwdp);
+
+    /* cleanup */
     memset(input, 1337, BUFFERSIZE);
+    putchar('\n');
+    fflush(stdout);
 
     /* compare hashes */
     if (strcmp(shadowEntry->sp_pwdp, hashed_pw)) {
